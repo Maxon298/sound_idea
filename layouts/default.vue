1e36fcf611d5e5431a55fe0ba6a83f7956a5b45e
Maxon298 <maxon298@yandex.ru>
2022-05-17 16:31:54 +0300

    create project

---
+++ layouts/default.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <header class="header">
+      <div class="header__content">
+        <div class="_container">
+          <div class="_row">
+            <div class="logo">
+              <img width='166' height='36' src="@/assets/img/svg/logo.svg" alt="Logo">
+            </div>
+            <div class="fast-access">
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="header__nav">
+        <div class="_container">
+          <div class="_row">
+            <div class="nav-item">
+              Acapellas
+            </div>
+            <div class="nav-item">
+              Loop & Samples
+            </div>
+            <div class="nav-item">
+              Presets
+            </div>
+            <div class="nav-item">
+              Projects
+            </div>
+            <div class="nav-item">
+              MIDI
+            </div>
+            <div class="nav-item">
+              VST plugins
+            </div>
+            <div class="nav-item">
+              FAQ
+            </div>
+            <div class="nav-item">
+              Community
+            </div>
+            <div class="nav-item">
+              Support/Donate
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <Nuxt class=''/>
+    <footer class="footer"></footer>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+  .header{
+    &__content{
+      background: #16162C;
+      height: 65px;
+    }
+    &__nav{
+      height: 35px;
+      ._row{
+        align-items: center;
+      }
+      background: #444674;
+      .nav-item{
+        color: white;
+        font-size: 10px;
+        &:not(:last-child){
+          margin-right: 16px;
+        }
+      }
+    }
+  }
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,63 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'sound_idea',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/logoimg.svg' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/assets/css/style.scss',
+    '@/static/Montserrat/stylesheet.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  router: {
+    prefetchLinks: false
+  },
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  styleResources: {
+    scss: ['./assets/css/*.scss']
+  },
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  server: {
+    port: 3009
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
